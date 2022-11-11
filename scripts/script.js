91769d78062ed6a0ff54d8222d22259bddd96abc
Veta Komolova <vetarejh@gmail.com>
2022-11-11 18:31:04 +0300

    исправлены микро моменты в верстке мобайл и добавлен скрипт открытия формы

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = document.querySelector('.close-button');
+const popupOpenButtonElement = document.querySelector('.edit-button');
+
+const openPopup = function() {
+  popupElement.classList.add('popup__open');
+}
+
+const closePopup = function() {
+  popupElement.classList.remove('popup__open');
+}
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+let formElement = document.querySelector('.popup');
+
+let FirstNameInput = document.querySelector('.profile__first-name');
+let LastNameInput = document.querySelector('.profile__last-name');
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей  lastNameInput и FirstNameInput из свойства value
+
+    FirstNameInput.value('.profile__first-name');
+    LastNameInput.value('.profile__last-name');
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
