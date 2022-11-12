5ebca861cba10590d12a1452441d3cd9d622bb01
Veta Komolova <vetarejh@gmail.com>
2022-11-12 19:55:32 +0300

    первая векрсия

--- scripts/script.js
+++ scripts/script.js
@@ -6,24 +6,24 @@ const jobInput = document.querySelector('.form__item_job');
 const nameText = document.querySelector('.profile__name');
 const jobText = document.querySelector('.profile__job');
 
-const openPopup = function() {
+const openPopup = function () {
   popupElement.classList.add('popup__open');
   nameInput.value = nameText.textContent;
   jobInput.value = jobText.textContent;
 }
 
-const closePopup = function() {
+const closePopup = function () {
   popupElement.classList.remove('popup__open');
 }
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    nameText.textContent = nameInput.value;
-    jobText.textContent = jobInput.value;
-    closePopup();
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  nameText.textContent = nameInput.value;
+  jobText.textContent = jobInput.value;
+  closePopup();
 }
 
 popupElement.addEventListener('submit', formSubmitHandler);
