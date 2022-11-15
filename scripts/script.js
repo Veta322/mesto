cf4336a3042b385ed7ea955e82c831172af14039
Veta Komolova <vetarejh@gmail.com>
2022-11-15 16:58:57 +0300

    осталось исправить 2 замечания

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,19 @@
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = document.querySelector('.close-button');
 const popupOpenButtonElement = document.querySelector('.edit-button');
-const nameInput = document.querySelector('.form__item_name');
-const jobInput = document.querySelector('.form__item_job');
+const nameInput = document.querySelector('.form__item_type_name');
+const jobInput = document.querySelector('.form__item_type_job');
 const nameText = document.querySelector('.profile__name');
 const jobText = document.querySelector('.profile__job');
 
 const openPopup = function () {
-  popupElement.classList.add('popup__open');
+  popupElement.classList.add('popup_open');
   nameInput.value = nameText.textContent;
   jobInput.value = jobText.textContent;
 }
 
 const closePopup = function () {
-  popupElement.classList.remove('popup__open');
+  popupElement.classList.remove('popup_open');
 }
 
 popupOpenButtonElement.addEventListener('click', openPopup);
